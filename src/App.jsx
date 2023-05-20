cf47887f3e223698f4c9a0b71013c17f9cb44e16
Agustin Fraile <agustinfraile98@gmail.com>
2023-05-20 13:15:00 -0300

    terminando estructura basica

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Home from "./pages/Home/Home";
 import Nav from "./components/Nav/Nav";
 
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Notes from "./pages/Notes/Notes";
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/notes" element={<Notes />} />
         </Routes>
+        <Footer />
       </div>
     </BrowserRouter>
   );
