f3464051af523ee44ad6c83f41390393355fb836
Agustin Fraile <agustinfraile98@gmail.com>
2023-05-20 15:47:44 -0300

    estilos para las notas

--- src/pages/Notes/Notes.jsx
+++ src/pages/Notes/Notes.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
 
+import styles from './Notes.module.css'
+
 const Notes = () => {
   return (
-    <div>Notes</div>
+    <div
+      className={styles.notesContainer}
+    >
+
+      <div
+        className={styles.notesInput}
+      >
+        <div
+          className={styles.notesInput_form}
+        >
+          <form className={styles.formContainer}>
+            <input 
+              className={styles.formContainer_input} 
+              type="text" 
+              placeholder='Agregar una tarea'  
+            />
+            <button 
+              type="submit"
+              className={styles.formContainer_submit}
+            >
+              +
+            </button>
+          </form>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
