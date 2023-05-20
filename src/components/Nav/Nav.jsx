747e0a33ccbe0d75dc5c9b79117de4d5a90c6209
Agustin Fraile <agustinfraile98@gmail.com>
2023-05-20 13:32:30 -0300

    navbar terminado

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./Nav.module.css";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <nav className={styles.navContainer}>
+      <ul className={styles.navContainer_ul}>
+        <Link to="/" className={styles.navContainer_link}>
+          <li className={styles.navContainer_list}>Inicio</li>
+        </Link>
+        <Link to="/notes" className={styles.navContainer_link}>
+          <li className={styles.navContainer_list}>Notas</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
