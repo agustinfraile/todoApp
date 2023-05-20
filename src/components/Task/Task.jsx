6fb255d1103eeb93abb4fa7ccd430f65900d82ad
Agustin Fraile <agustinfraile98@gmail.com>
2023-05-20 18:06:49 -0300

    agregando botones principales, estilos y funcionalidad a delete

---
+++ src/components/Task/Task.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+
+import styles from "./Task.module.css";
+
+const Task = ({ tasks, onDeleteTask }) => {
+
+
+  return (
+    <div className={styles.taskContainer}>
+        
+        <div className={styles.completeTask}>
+            <div className={styles.completeBtn}>
+                <RadioButtonUncheckedIcon className={styles.completeIcon}/>
+            </div>
+        </div>
+
+        <div className={styles.taskContainer_name}>
+            <h3 className={styles.taskContainer_name__h3}>
+                {tasks.name}
+            </h3>
+        </div>
+
+        <div className={styles.taskContainer_buttons}>
+            <div className={styles.taskContainer_buttons_delete}>
+                <div 
+                    className={styles.deleteBtn}
+                    onClick={() => onDeleteTask(tasks.id)}
+                >
+                    <DeleteIcon 
+                        className={styles.editIcon}    
+                    />
+                </div>
+            </div>
+            <div className={styles.taskContainer_buttons_editar}>
+                <div
+                    className={styles.editBtn}
+                >
+                    <EditIcon className={styles.editIcon}/>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Task;
--- src/pages/Notes/Notes.jsx
+++ src/pages/Notes/Notes.jsx
@@ -1,37 +1,74 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import styles from './Notes.module.css'
+import Task from "../../components/Task/Task";
+
+import styles from "./Notes.module.css";
 
 const Notes = () => {
-  return (
-    <div
-      className={styles.notesContainer}
-    >
+  const [task, setTask] = useState({
+    name: '',
+    id: Date.now()
+  });
+  const [allTask, setAllTask] = useState([]);
 
-      <div
-        className={styles.notesInput}
-      >
-        <div
-          className={styles.notesInput_form}
-        >
-          <form className={styles.formContainer}>
-            <input 
-              className={styles.formContainer_input} 
-              type="text" 
-              placeholder='Agregar una tarea'  
+  const handleInputChange = (e) => {
+    setTask((prevTask) => ({
+      ...prevTask,
+      name: e.target.value
+    }))
+  };
+
+  // console.log(allTask);
+
+  const onDeleteTask = (id) => {
+    const filteredTask = allTask.filter(task => task.id !== id);
+    setAllTask(filteredTask);
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (task.name.trim() !== "") {
+      setAllTask((prevTask) => [...prevTask, task]);
+      setTask({
+        name: '',
+        id: Date.now()
+      });
+    }
+  };
+
+  return (
+    <div className={styles.notesContainer}>
+      <div className={styles.notesInput}>
+        <div className={styles.showInputs}>
+          {allTask.map((tasks) => (
+            <Task 
+              key={tasks.id} 
+              tasks = {tasks}
+              onDeleteTask={onDeleteTask}
             />
-            <button 
-              type="submit"
-              className={styles.formContainer_submit}
-            >
+          ))}
+        </div>
+
+        <div className={styles.notesInput_form}>
+          <form onClick={handleSubmit} className={styles.formContainer}>
+            <input
+              className={styles.formContainer_input}
+              type="text"
+              name="task"
+              value={task.name}
+              placeholder="Agregar una tarea"
+              onChange={(e) => handleInputChange(e)}
+            />
+            <button type="submit" className={styles.formContainer_submit}>
               +
             </button>
           </form>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Notes
+export default Notes;
